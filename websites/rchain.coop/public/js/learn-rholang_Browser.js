ef81f9c1d3f8009447208310dbd5b2277177ebc6
Steve <steve@pyrofex.net>
2018-08-30 11:16:50 -0600

    n/a

--- websites/rchain.coop/public/js/learn-rholang_Browser.js
+++ websites/rchain.coop/public/js/learn-rholang_Browser.js
@@ -10,3 +10,12 @@ document.getElementById("close").onclick = contentsCollapse;
 function contentsCollapse() {
     document.getElementById("sideNav").classList.remove("active");
 }
+
+
+$(".scroll").click(function () {
+    var scrollId = $(this).attr("id");
+    $('html, body').animate({
+        scrollTop: ($("#" + scrollId + "Section").offset().top - 100)
+    });
+    return false;
+});
